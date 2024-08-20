f36ade6b09750de5f6f955198c067349f0c0604c
Manish <[manishbulchandani4@gmail.com]>
2024-08-20 12:24:27 +0530

    UI side fixes

--- client/src/pages/user/UserLayout.tsx
+++ client/src/pages/user/UserLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
 import BottomNav from "../../components/user/BottomNav"
-import { Stack } from "@mui/material"
+import { Box, Stack } from "@mui/material"
 
 const UserLayout = () => {
   return (
     <Stack>
       <Outlet/>
+      <Box bgcolor="transparent" height="58px"/>
       <BottomNav/>
     </Stack>
   )
