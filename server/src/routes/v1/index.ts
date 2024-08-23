deb1c70f7f6cd629f61970f07a3b2fa67d30834c
IshikaT21 <ishikatiwari416@gmail.com>
2024-08-23 21:38:25 +0530

    Added API of top scorers on weekly basis

--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoutes from "../../routes/v1/user.route";
 import questionRoutes from "../../routes/v1/user.route";
+import adminRoutes from "../../routes/v1/admin.route";
 const router = express.Router();
 
 router.use("/users", userRoutes);
 router.use("/question", questionRoutes);
+router.use("/admin", adminRoutes);
 
 export default router;
