81d787574738116d114884e86f6acb42e6036bd6
IshikaT21 <ishikatiwari416@gmail.com>
2024-08-31 13:09:21 +0530

    Added Current User score details.

--- server/src/controllers/scoreboard.controller.ts
+++ server/src/controllers/scoreboard.controller.ts
@@ -13,17 +13,20 @@ export const handleScoreboard = async (
     return;
   }
 
-  const users = await User.find({}).select("_id");
+  const users = await User.find({})
+    .select("_id score name")
+    .sort({ score: -1 })
+    .limit(10);
 
   const scoreboard = [];
-
+  let rank = 1;
   for (const user of users) {
     const weeklyResponses = await WeeklyResponse.find({ user: user._id });
 
     if (!weeklyResponses) {
       return next(new AppError("There are no responses from this user", 400));
     }
-    let scoreTotal = 0;
+
     const totalTimeEngaged = [];
     for (const weeklyResponse of weeklyResponses) {
       const startDate = new Date(weeklyResponse.startTime);
@@ -36,8 +39,6 @@ export const handleScoreboard = async (
         timeDifferenceInMilliseconds / (1000 * 60);
 
       totalTimeEngaged.push(timeDifferenceInMinutes);
-
-      scoreTotal += weeklyResponse.score;
     }
 
     const totalEngagedTimeOfUser = totalTimeEngaged.reduce(
@@ -47,15 +48,74 @@ export const handleScoreboard = async (
 
     const response = [
       {
-        userName: user.name,
-        user: user._id,
-        time: totalEngagedTimeOfUser,
-        points: scoreTotal,
+        rank: rank,
+        name: user.name,
+        timeInMints: totalEngagedTimeOfUser,
+        points: user.score,
       },
     ];
 
     scoreboard.push(response);
+    rank += 1;
   }
 
-  res.status(200).json({ scoreboard });
+  const { _id: userId } = req.user;
+  const currentUserScore = await User.findOne({ _id: userId }).select(
+    "name score"
+  );
+
+  if (!currentUserScore) {
+    scoreboard.push({
+      user: userId,
+      name: req.user.name,
+      score: 0,
+    });
+    return;
+  }
+  const weeklyResponses = await WeeklyResponse.find({ user: userId });
+
+  if (!weeklyResponses) {
+  }
+
+  const totalUserTime = [];
+  for (const weeklyResponse of weeklyResponses) {
+    const startDate = new Date(weeklyResponse.startTime);
+    const endDate = new Date(weeklyResponse.endTime);
+
+    const timeDifferenceInMilliseconds: number =
+      endDate.getTime() - startDate.getTime();
+
+    const timeDifferenceInMinutes: number =
+      timeDifferenceInMilliseconds / (1000 * 60);
+
+    totalUserTime.push(timeDifferenceInMinutes);
+  }
+
+  const totalEngagedTimeOfCurrentUser = totalUserTime.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  let currentUserRank = -1;
+  const userRanks = await User.find({})
+    .select("_id score name")
+    .sort({ score: -1 });
+
+  for (const userRank of userRanks) {
+    if (userRank._id == userId) {
+      currentUserRank = 1;
+    }
+    currentUserRank += 1;
+  }
+
+  console.log("currentUserRank: ", currentUserRank);
+
+  const currentUserDetails = {
+    rank: currentUserRank,
+    name: "You",
+    timeInMints: totalEngagedTimeOfCurrentUser,
+    score: currentUserScore?.score,
+  };
+
+  res.status(200).json({ scoreboard, currentUserDetails });
 };
