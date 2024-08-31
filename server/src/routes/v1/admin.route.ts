864f96e733f183561ad4bd6522606e96ef01a6c9
Manish <[manishbulchandani4@gmail.com]>
2024-08-31 21:33:38 +0530

    Typo fix at backend

--- server/src/routes/v1/admin.route.ts
+++ server/src/routes/v1/admin.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import asyncHandler from "../../utils/asyncHandler";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 import { handleAdminDashboard } from "../../controllers/adminDashboard.controller";
-import { employeeStats } from "../../controllers/employeeStats.Controller";
+import { employeeStats } from "../../controllers/employeeStats.controller";
 const router = express.Router();
 
 router
